227b5e41681db4a18574c65e6ddb6bec4bf9b4f8
Joel <jcsilberman@gmail.com>
2021-03-04 11:12:53 -0500

    Moved files
    Co-authored-by: Ethan Strominger <ethanstrominger@gmail.com>

---
+++ Java/src/com/ethan/Car.java
@@ -0,0 +1,17 @@
+package com.ethan;
+
+import java.util.UUID;
+
+public class Car {
+    public final String color;
+    public String id;
+
+
+    public Car(String color) {
+        this.color = color;
+        this.id = UUID.randomUUID().toString();
+    }
+
+
+}
+
